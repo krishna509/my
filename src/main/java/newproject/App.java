9f352ae5bfec74df3184133751c8748eeabf32ce
krishna509 <59523364+krishna509@users.noreply.github.com>
2020-01-12 18:16:02 +0530

    updatedapp.java

--- src/main/java/newproject/App.java
+++ src/main/java/newproject/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         
-        // nice work done
+        // nice work done how is i
         System.out.println( "Hello World!" );
     }
 }
